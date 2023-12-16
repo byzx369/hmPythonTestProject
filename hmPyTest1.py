877aa8011ceab4decb1a823c4640cd59428044bb
byzx369 <andreaw1120@gmail.com>
2023-12-16 14:18:48 -0800

    Edited the hmPyTest1.py

--- hmPyTest1.py
+++ hmPyTest1.py
@@ -2,22 +2,22 @@
 
 import json
 import unittest
-from parameterized import parameterized
+# from parameterized import parameterized
 from hm192tools import login
 from tools import add
 
-def build_data():
-    with open('data.json', encoding="utf-8")as f:
-        result=json.load(f)  ###[{},{},{}]
-        data=[]
-        for i in result: ### i={}
-            data.append((i.get('username'), i.get('password'), i.get('expect')))
-    return data
-
-class TestLogin(unittest.TestCase):
-    @parameterized.expand(build_data())
-    def test_login(self, username, password, expect):
-        self.assertEqual(expect, login(username,password))
+# def build_data():
+#     with open('data.json', encoding="utf-8")as f:
+#         result=json.load(f)  ###[{},{},{}]
+#         data=[]
+#         for i in result: ### i={}
+#             data.append((i.get('username'), i.get('password'), i.get('expect')))
+#     return data
+# 
+# class TestLogin(unittest.TestCase):
+#     @parameterized.expand(build_data())
+#     def test_login(self, username, password, expect):
+#         self.assertEqual(expect, login(username,password))
 
 version=30
 class TestDemo(unittest.TestCase):
